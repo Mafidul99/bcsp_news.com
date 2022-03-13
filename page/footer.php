8115105132faa27bbdd624d99c3df74dc0748dd4
Mafidul99 <mdonline192@gmail.com>
2022-03-13 15:53:30 +0530

    first commit

---
+++ page/footer.php
@@ -0,0 +1,70 @@
+<footer class="footer-wrapper bg-dark">
+        <div class="footer-widgets padding">
+            <div class="container">
+                <div class="col-md-4 col-xs-6 sm-padding">
+                    <div class="single-widget">
+                        <a href="#" class="footer-logo"><img src="img/logo/footer_logo.png" alt="News24"></a>
+                        <p class="text-light no-margin">Peindia News brings latest news from India and World on breaking news, today news headlines, politics, business, technology, bollywood, entertainment ..</p>
+                    </div>
+                </div>
+                <!-- widget 1 -->
+                <div class="col-md-4 col-xs-6 sm-padding">
+                    <div class="single-widget">
+                        <h3>Most Viwed</h3>
+                        <div class="footer-posts">
+                            <div class="footer-post">
+                                <span class="date">Oct 28, 2016</span>
+                                <h4><a href="#">Success is not a good teacher failure makes you humble</a></h4>
+                            </div>
+                            <div class="footer-post">
+                                <span class="date">Oct 28, 2016</span>
+                                <h4><a href="#">Success is not a good teacher failure makes you humble</a></h4>
+                            </div>
+                        </div>
+                        <!-- /.footer-posts -->
+                    </div>
+                </div>
+                <!-- widget 2 -->
+                <div class="col-md-4 col-xs-12 sm-padding">
+                    <div class="single-widget">
+                        <h3>Most Viwed</h3>
+                        <ul class="tag-list">
+                            <li><a href="#">Worlds</a></li>
+                            <li><a href="#">Entertainment</a></li>
+                            <li><a href="#">Health</a></li>
+                            <li><a href="#">Football</a></li>
+                            <li><a href="#">News</a></li>
+                            <li><a href="#">Fasion</a></li>
+                            <li><a href="#">Health</a></li>
+                            <li><a href="#">Western Foods</a></li>
+                            <li><a href="#">Western Foods</a></li>
+                            <li><a href="#">Race</a></li>
+                            <li><a href="#">Football</a></li>
+                            <li><a href="#">Foods</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- widget 3 -->
+            </div>
+        </div>
+        <!-- /.footer-widgets -->
+        <div class="footer-copyright ptb-40">
+            <div class="container">
+                <div class="col-xs-6 xs-padding xs-text-center">
+                    <p class="no-margin fz-13 text-light">&copy; Copyright <a class="fooetrtext" href="#">Peindia News</a> 2022, All Rights Reserved</p>
+                </div>
+                <div class="col-xs-6 xs-padding">
+                    <ul class="footer-social text-right xs-text-center">
+                        <li><a href="#"><i class="ti-facebook"></i></a></li>
+                        <li><a href="#"><i class="ti-twitter"></i></a></li>
+                        <li><a href="#"><i class="ti-google"></i></a></li>
+                        <li><a href="#"><i class="ti-pinterest"></i></a></li>
+                        <li><a href="#"><i class="ti-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- /.footer-wrapper -->
+
+    <a data-scroll href="#header" id="scroll-top" class="scroll-to-top"><i class="ti-angle-up"></i></a>
---
+++ page/header.php
@@ -0,0 +1,95 @@
+<header id="header" class="header sticky-header">
+        <div class="top-header bg-dark">
+            <div class="container">
+                <div class="col-md-6">
+                    <ul class="top-info">
+                        <li><i class="ti-mobile"></i> 1800300030030</li>
+                        <li><a href="mailto:support@peindianews.com"><i class="ti-email"></i> support@peindianews.com</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 text-right sm-text-left">
+                    <ul class="top-social">
+                        <li><a href="#"><i class="ti-facebook"></i></a></li>
+                        <li><a href="#"><i class="ti-twitter"></i></a></li>
+                        <li><a href="#"><i class="ti-google"></i></a></li>
+                        <li><a href="#"><i class="ti-pinterest"></i></a></li>
+                        <li><a href="#"><i class="ti-instagram"></i></a></li>
+                        <li class="fz-13">Thousday Jan 5 , 2022</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- /.top-header -->
+        <div class="mid-header">
+            <div class="container">
+                <div class="site-branding full-left">
+                    <a href="index.php"><img src="img/logo/logo_news.png" alt="Peindianews"></a>
+                </div>
+                <!-- /.site-branding -->
+                <div class="banner pull-right">
+                    <a href="#"><img src="img/banner.jpg" alt="Peindianews Banner"></a>
+                </div>
+                <!-- /.banner -->
+            </div>
+        </div>
+        <!-- /.mid-header -->
+        <div class="mainmenu-wrap bg-red">
+            <div class="container">
+                <div class="col-md-9 col-xs-6">
+                    <div class="menu-wrap mean-menu">
+                        <nav class="mainmenu">
+                            <ul id="navi-menu">
+                                <li class="current_page_item"><a href="index.php">Home</a></li>
+                                <li><a href="#">Health</a>
+                                    <ul>
+                                        <li><a href="news-single.html">News Health1</a></li>
+                                        <li><a href="news-single.html">News Health2</a></li>
+                                        <li><a href="news-single.html">News Health3</a></li>
+                                        <li><a href="news-single.html">News Health4</a></li>
+                                        <li><a href="news-single.html">News Health5</a></li>
+                                    </ul>
+                                    </li>
+                                <li><a href="#">Entertainment</a>
+                                    <ul>
+                                        <li><a href="news-single.html">News Entertainment1</a></li>
+                                        <li><a href="news-single.html">News Entertainment2</a></li>
+                                        <li><a href="news-single.html">News Entertainment3</a></li>
+                                        <li><a href="news-single.html">News Entertainment4</a></li>
+                                        <li><a href="news-single.html">News Entertainment5</a></li>
+                                    </ul>
+                                    </li>
+                                <li><a href="#">World</a>
+                                    <ul>
+                                        <li><a href="news-single.html">News World1</a></li>
+                                        <li><a href="news-single.html">News World2</a></li>
+                                        <li><a href="news-single.html">News World3</a></li>
+                                        <li><a href="news-single.html">News World4</a></li>
+                                        <li><a href="news-single.html">News World5</a></li>
+                                    </ul>
+                                    </li>
+                                    <li><a href="#">Project</a>
+                                    <ul>
+                                        <li><a href="news-single.html">News Project1</a></li>
+                                        <li><a href="news-single.html">News Project2</a></li>
+                                        <li><a href="news-single.html">News Project3</a></li>
+                                        <li><a href="news-single.html">News Project4</a></li>
+                                        <li><a href="news-single.html">News Project5</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="col-md-3 col-xs-6">
+                    <form action="#" class="search-form">
+                        <input type="text" placeholder="Search news..">
+                        <button class="submit" type="submit"><i class="ti-search"></i></button>
+                    </form>
+                </div>
+                <div class="mobile-menu"></div>
+            </div>
+        </div>
+        <!-- /.mainmenu-wrap -->
+    </header>
