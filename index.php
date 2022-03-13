445d641485ebbdba2cf8996adacf64f977285aaf
Mafidul99 <mdonline192@gmail.com>
2022-03-13 17:20:07 +0530

    gg

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
-<!doctype html>
+<!DOCTYPE html>
 <html class="no-js" lang="en">
-
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Favicon -->
     <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
     <title>Peindia Live - News Protal</title>
